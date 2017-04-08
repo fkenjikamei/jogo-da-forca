d3e1719f1295deb350b3beb331add85ca5afdc44
guimaraf <guimaraf@gmail.com>
2017-04-08 11:22:49 -0300

    Nova versão Forca, atualizado json

---
+++ jogodaForcav2.py
@@ -0,0 +1,102 @@
+import random
+import json
+import os 
+
+letrasUsadas = "" #serve para armazenar as letras que já foram digitadas 
+caracterespermitidos = "abcdefghijklmnopqrstuvwxyz" #limita os caracteres usados no código
+palavraEscolhida = ""
+letrasDescobertas = []
+limiteErros = 6
+acertoLetras = 0
+erroLetras = 0
+
+data = json.load(open('data.json')) # carrega dados do arquivo json
+os.system('cls' if os.name == 'nt' else 'clear') #Vai limpar tela, tanto no windows ou linux
+nome = str(input("Digite seu nome: "))
+print("\n*** Jogo da Forca em Python ***") # titulo do jogo
+print("*** Bem vindo {nome} ao jogo ***\n".format(nome=nome)) # mensagem de boas vidas
+print("\n*** Escolha uma categoria abaixo ***") # imprime a lista de categorias contida no arquivo JSON
+
+for item in data: 
+	print('*',item) # pergunta a categoria
+
+verificaCategoria = False
+while(verificaCategoria == False):
+    categoria = str(input("Digite a categoria: ")).lower()
+    if(categoria == "frutas" or categoria == "nomes"):
+        verificaCategoria = True
+    else:
+        print("Nome da categoria inválido")
+    
+palavraEscolhida = data[categoria][random.randrange(0, len(data[categoria]))] # sorteia a palavra secreta a ser descobrerta
+    
+for i in range(0, len(palavraEscolhida)): # se a palavra ainda não foram descobertas adiciona um "-"
+        letrasDescobertas.append("-")
+
+print(letrasDescobertas)
+
+def entradaTeclado(): 
+    verificaLetra = False
+    verificaRepetida = False
+    verificarNaPalavraSecreta = False
+    global letrasUsadas #Estou usando variáveis globais na função para evitar ficar passando por parâmetros, o que pode deixar o código um pouco complexo
+    global erroLetras
+    global acertoLetras
+    global letrasDescobertas
+    
+    entradaLetra = input("Digite a letra: ").lower()
+    for i in range(0, len(caracterespermitidos)): #Será verificado se a letra é válida, dentro do escopo inicial de letras permitidas
+        if (entradaLetra == caracterespermitidos[i]):
+            print("Caractere encontrado")
+            verificaLetra = True
+    
+    if (verificaLetra == True): #Agora é verificado se a letra já foi digitada pelo usuário antes
+        for i in range(0, len(letrasUsadas)):
+            if (entradaLetra == letrasUsadas[i]):
+                print("Letra já usada, digite novamente") #Evitar de registrar caractere repetido
+                verificaRepetida = True
+                entradaTeclado()
+        
+        if(verificaRepetida == False): #Se cair neste bloco, é por não ser repetida
+            letrasUsadas += entradaLetra
+            
+            for i in range(0, len(palavraEscolhida)):
+                if(entradaLetra == palavraEscolhida[i]): #Verifica se a letra digitada existe na palavra escolhida
+                    verificarNaPalavraSecreta = True
+                    acertoLetras += 1
+                    letrasDescobertas[i] = entradaLetra #Vai preencher visual as letras
+            
+            os.system('cls' if os.name == 'nt' else 'clear') #Vai limpar tela, tanto no windows ou linux
+            reserva = "" #Criada apenas para exibir em linha
+            for i in range(0, len(palavraEscolhida)): #Vai passar dentro da palavra secreta para desenhar na tela
+                reserva += letrasDescobertas[i]
+            print(reserva)
+    
+            if (verificarNaPalavraSecreta == True):
+                print("Existe a letra", entradaLetra,"\nPróxima letra")
+
+            else:
+                print("Não existe a {letra} na palavra secreta".format(letra=entradaLetra))
+                erroLetras += 1
+        else:
+            print("Letras já digitadas", letrasUsadas)
+            entradaTeclado()
+
+    if (verificaLetra == False):
+        print("Caractere inválido ")
+        entradaTeclado()
+
+while (erroLetras < limiteErros and acertoLetras < len(palavraEscolhida)): #Definição do que pode dar game over e vitória
+    print("Letras já digita", letrasUsadas)
+    print("Erros", erroLetras)
+    print("Acertos", acertoLetras)
+    
+    if (erroLetras == 5):
+        print("Última tentativa, próximo erro, perde o jogo. ")
+    entradaTeclado()
+
+if(acertoLetras >= len(palavraEscolhida) and acertoLetras > 0): #Resposta para a condição de game over
+    print("Parabéns, você descobriu a palavra secreta", palavraEscolhida)
+
+if(erroLetras >= limiteErros): #Resposta para a condição de vitória
+    print("Infelizmente atingiu o limite de erros")
