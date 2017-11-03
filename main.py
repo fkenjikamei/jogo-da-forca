dafcdddbcbf443b1b44abe148810a6c9c6f8a1c9
Fernando Kenji Kamei <fkenjikamei@gmail.com>
2017-11-02 21:33:44 -0300

    Melhorando a legibilidade do código
    Comecei a separar o código em funções para facilitar a manutenção.

--- main.py
+++ main.py
@@ -8,26 +8,39 @@ letrasDescobertas = []
 limiteErros = 6
 acertoLetras = 0
 erroLetras = 0
+data = ""
+desenhoTela = ""
+categoria = ""
+verificaCategoria = False
 
-desenhoTela = Imprimir()
-os.system('cls' if os.name == 'nt' else 'clear') #Vai limpar tela, tanto no windows ou linux
-data = json.load(open('data.json')) # carrega dados do arquivo json
+
+def configuracoesIniciaisDoJogo():
+	global data, desenhoTela
+	desenhoTela = Imprimir()
+	os.system('cls' if os.name == 'nt' else 'clear') #Vai limpar tela, tanto no windows ou linux
+	data = json.load(open('data.json')) # carrega dados do arquivo json
+
+def escolherCategoria():
+	global verificaCategoria, categoria
+	while verificaCategoria == False:
+	    categoria = input("\nDigite a categoria: ").lower()
+	    
+	    if(categoria == "frutas" or categoria == "nomes"):
+	        verificaCategoria = True
+	    else:
+	        print("Nome da categoria inválida")
+
+###### INÍCIO DO JOGO ######
+configuracoesIniciaisDoJogo()
 nome = input("Digite seu nome: ")
 print("\n*** Jogo da Forca em Python ***") # titulo do jogo
 print("*** Bem vindo ao jogo {} ***\n".format(nome)) # mensagem de boas vidas
 print("\n*** Escolha uma categoria abaixo ***") # imprime a lista de categorias contida no arquivo JSON
-
 for item in data:
 	print('*',item) # pergunta a categoria
 
-verificaCategoria = False
-while verificaCategoria == False:
-    categoria = input("\nDigite a categoria: ").lower()
-    
-    if(categoria == "frutas" or categoria == "nomes"):
-        verificaCategoria = True
-    else:
-        print("Nome da categoria inválida")
+escolherCategoria()
+
 
 
 palavraEscolhida = data[categoria][random.randrange(0, len(data[categoria]))] # sorteia a palavra secreta a ser descobrerta
@@ -46,18 +59,17 @@ def entradaTeclado():
     reserva = "" #Criada apenas para exibir em linha
     global letrasUsadas, erroLetras, acertoLetras, letrasDescobertas
 
-    entradaLetra = input("Digite a letra: ").lower()
+    entradaLetra = input("\nDigite a letra: ").lower()
     if(len(entradaLetra) == 1):
 
         for i in range(0, len(caracteresPermitidos)): #Será verificado se a letra é válida, dentro do escopo inicial de letras permitidas
             if (entradaLetra == caracteresPermitidos[i]):
-                print("Caractere encontrado")
                 verificaLetra = True
 
     if verificaLetra == True: #Agora é verificado se a letra já foi digitada pelo usuário antes
         for i in range(0, len(letrasUsadas)):
             if entradaLetra == letrasUsadas[i]:
-                print("Letra já usada, digite novamente") #Evitar de registrar caractere repetido
+                print(">>> Letra já usada, digite novamente") #Evitar de registrar caractere repetido
                 verificaLetraRepetida = True
                 entradaTeclado()
 
@@ -85,17 +97,18 @@ def entradaTeclado():
     os.system('cls' if os.name == 'nt' else 'clear') #Vai limpar tela, tanto no windows ou linux
     desenhoTela.desenharNaTela(erroLetras)
     print("Letras já digitadas: ", letrasUsadas)
+    
     for i in range(0, len(palavraEscolhida)): #Vai passar dentro da palavra secreta para desenhar na tela
         reserva += letrasDescobertas[i]
     print("\n", reserva)
 
 while erroLetras < limiteErros and acertoLetras < len(palavraEscolhida): #Definição do que pode dar game over e vitória
     if (erroLetras == 5):
-        print("Última tentativa, próximo erro, perde o jogo. ")
+        print(">>> Última tentativa, próximo erro, perde o jogo. ")
     entradaTeclado()
 
 if acertoLetras >= len(palavraEscolhida) and acertoLetras > 0: #Resposta para a condição de vitória
-    print("Parabéns", nome, "você descobriu a palavra secreta", palavraEscolhida)
+    print("*** Parabéns", nome, "você descobriu a palavra secreta", palavraEscolhida)
 
 if erroLetras >= limiteErros: #Resposta para a condição de game over
-    print(nome, "infelizmente você atingiu o limite de erros, a palavra secreta é", palavraEscolhida)
+    print("!!! ", nome, "infelizmente você atingiu o limite de erros, a palavra secreta é", palavraEscolhida)
